7d7e79e9a348e675254c4fac98db7e16a004a86a
Eric Washington <eric.washington.me@gmail.com>
2021-09-19 18:10:11 -0400

    Initial Commmit

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: "What is your Full Name?",
+    answer: "Eric A. Washington",
+  },
+  {
+    question: "What city do you currently live in?",
+    answer: "Washington, DC",
+  },
+  {
+    question: "What is your favorite programing languauge",
+    answer: "Javascript",
+  },
+];
